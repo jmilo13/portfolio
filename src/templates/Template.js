bf9fc505013093848e29ccafeec16c86329f6310
Camilo <camilogm3@gmail.com>
2021-04-08 20:12:13 -0300

    modificación con API R&M

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -11,19 +11,19 @@ const Template = async () => {
       <div class="card">
         <div class="card_details">
           <div class="card_photo center circle">
-            <img src="${data.picture.large}" alt="${data.name.first}">
+            <img src="${data.image}" alt="${data.name}">
             <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
               xml:space="preserve">
               <circle cx="50" cy="50" r="40" />
             </svg>
           </div>
           <p class="card_title">Hi, My name is</p>
-          <p class="card_value">${data.name.first} ${data.name.last}</p>
+          <p class="card_value">${data.name}</p>
         </div>
         <div class="card_userdata">
           <ul>
-            <li>${data.email}</li>
-            <li>${data.location.country}</li>
+            <li>${data.status}</li>
+            <li>${data.origin.name}</li>
           </ul>
         </div>
         <div class="card_social">
